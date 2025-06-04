326737fa95e9b79e07ba901e2bc9a84255355fd4
Evgeniy Mokich <mokichzhenya@gmail.com>
2025-06-04 17:17:37 +0200

    fixed viewmodel dispatcher koin scope and increased triggerAtMillis for location service alarm fallback

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.suit.silentsync"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1-alpha"
+        versionCode = 5
+        versionName = "1.1-alpha01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
